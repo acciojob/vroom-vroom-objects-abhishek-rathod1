8287b6733f12b9bd60220efc003832c51f778686
Abhishek Rathod <118195093+abhishek-rathod1@users.noreply.github.com>
2025-04-13 12:51:11 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,7 +1,23 @@
 // Complete the js code
-function Car(make, model) {}
+function Car(make, model) {
+	constructor(make, model){
+		this.make = make;
+		this.model = model;
+	}
+	getMakeMode(){
+		return `${this.make} ${this.model}`;
+	}
+}
 
-function SportsCar(make, model, topSpeed) {}
+function SportsCar(make, model, topSpeed) extends Car {
+	constructor(make, model, topSpeed){
+		super(make, model);
+		this.topspeed = topSpeed
+	}
+	getTopSpeed(){
+		return this.topSpeed;
+	}
+}
 
 // Do not change the code below
 window.Car = Car;
